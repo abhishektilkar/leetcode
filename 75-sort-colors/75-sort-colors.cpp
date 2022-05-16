1aba23b4447f9e65c228f08d19f8e59fa646da3b
Abhishek Tilkar <84792257+abhishektilkar@users.noreply.github.com>
2022-05-16 18:07:09 +0530

    Time: 3 ms (58.20%), Space: 8.1 MB (99.59%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -6,22 +6,39 @@ public:
         // for(int i) //
         
         int i=0;
+        
         int j=n-1;
+        
         int p=0;
         
         while(p <= j) {
+            
             if(nums[p] == 0) {
+                
                 swap(nums[i],nums[p]);
+                
                 i++;
+            
                 p++;
+            
             }
+            
             else if(nums[p] == 1) {
+            
                 p++;
+            
             }
+            
             else if(nums[p] == 2) {
+                
                 swap(nums[p],nums[j]);
+
                 j--;
+            
             }
+            
         }
+        
     }
+    
 };
