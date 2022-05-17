045b7c617db46fd95c22d4ba5ff92e8fd892cf1a
Abhishek Tilkar <84792257+abhishektilkar@users.noreply.github.com>
2022-05-17 12:12:56 +0530

    Time: 1390 ms (5.01%), Space: 247.1 MB (5.16%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -12,28 +12,46 @@ class Solution {
 public:
     
     TreeNode* value1 = NULL;
+    string path1 = "";
     
-    void valueNodE(TreeNode* cloned, int values) {
-        
-        if(cloned == NULL) {
-            return;
-        }
-        valueNodE(cloned->left, values);
-        if(cloned->val == values) value1 = cloned;
-        valueNodE(cloned->right, values);
-        
-    }
+   void original1(TreeNode* original, TreeNode* target,string path) {
+       
+       if(original == NULL) {
+           return;
+       }
+       
+       original1(original->left, target, path+"L");
+       
+       
+       if(original == target) {
+           path = path+"I";
+           path1 = path;
+           return;
+       }
+       
+       original1(original->right, target, path+"R");
+
+       
+   }
     
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
         
-        if(cloned == NULL) {
-            return NULL;
+        original1(original, target, "");
+    
+        cout<<path1;
+        
+        for(int i=0;i<path1.size();i++) {
+            if(path1[i] == 'I') {
+                return cloned;
+            }
+            else if(path1[i] == 'L') {
+                cloned = cloned->left;
+            }
+            else if(path1[i] == 'R') {
+                cloned = cloned->right;
+            }
         }
         
-        int value = target->val;
-        
-        valueNodE(cloned, value);
-        
-        return value1;
+        return cloned;
     }
 };
