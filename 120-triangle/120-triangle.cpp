ad426b292dfa21c6c0670517629417238454ec80
Abhishek Tilkar <84792257+abhishektilkar@users.noreply.github.com>
2022-06-13 18:20:51 +0530

    Time: 10 ms (43.76%), Space: 8.6 MB (91.19%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,36 +1,16 @@
 class Solution {
 public:
-    vector<vector<int>> v;
-    
-    int minimumTotalhelper(vector<vector<int>>& triangle,int i,int j) {
-        if(v[i][j] != -1) {
-            return v[i][j];
-        }
-        
-        if(i == triangle.size()-1) {
-            return v[i][j] = triangle[i][j];
-        }
-        
-        int valuej = minimumTotalhelper(triangle,i+1,j);
-        int valuej1 = minimumTotalhelper(triangle,i+1,j+1);
-        
-        return v[i][j] = (min(valuej,valuej1) + triangle[i][j]);
-        
-    }
-    
     int minimumTotal(vector<vector<int>>& triangle) {
-        
         int n = triangle.size();
+        vector<int> v = triangle[n-1];
         
-        v.resize(n,vector<int> (n,-1));
-        return minimumTotalhelper(triangle,0,0);
+        for(int i=n-2;i>=0;i--) {
+            for(int j=0;j<triangle[i].size();j++) {
+                v[j] = min(v[j],v[j+1]) + triangle[i][j];
+            }
+        }
         
+        return v[0];
+            
     }
-
-
-
-
-
-
-
 };
