20631b21f9bd1a63bec072801f9347054049a98e
Abhishek Tilkar <84792257+abhishektilkar@users.noreply.github.com>
2022-06-18 15:54:46 +0530

    Time: 838 ms (35.83%), Space: 154.8 MB (58.21%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -1,3 +1,87 @@
+// i
+struct Node{
+    Node *links[26];
+    bool lag;
+    
+    bool contains(char Ch) {
+        return (links[Ch-'a'] != NULL);
+    }
+    
+    Node* getrefrence(char Ch){
+        return links[Ch-'a'];
+    }
+    
+    void put(char Ch, Node* node) {
+        links[Ch-'a'] = node;
+    }
+    
+    void setone(){
+        lag = 1;
+    }
+    
+    bool isone(){
+        return (lag == 1);
+    }
+};
+class Trie {
+    private: Node * root;
+public:
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node *node = root;
+        
+        int N = word.size();
+        int i=0;
+        
+        while(i < N){
+            
+            if(!node->contains(word[i])){
+                node->put(word[i],new Node());
+            }
+            node = node->getrefrence(word[i]);
+            i++;
+        }
+        
+        node->setone();
+    }
+    
+    bool search(string word) {
+        Node* node = root;
+        
+        int i=0;
+        int N = word.size();
+        
+        while(i < N) {
+            if(!node->contains(word[i])){
+                return 0;
+            }
+            node = node->getrefrence(word[i]);
+            i++;
+        }
+        return (node->isone());
+    }
+    
+    bool startsWith(string word) {
+        Node *node = root;
+        
+        int N = word.size();
+        int i=0;
+        
+        while(i < N) {
+            if(!node->contains(word[i])){
+                return 0;
+            }
+            node = node->getrefrence(word[i]);
+            i++;
+        }
+        return 1;
+    }
+};
+
+
 class WordFilter {
 public:
     
@@ -9,10 +93,10 @@ public:
         for(string word : words) {
             index++;
             
-            for(int i=0;i<word.size();i++) {
+            for(int i=0;i<=word.size();i++) {
                 
                 for(int j=0;j<word.size();j++) {
-                    string toput = word.substr(0,i+1) + "#" + word.substr(j);
+                    string toput = word.substr(0,i) + "#" + word.substr(j);
                     map[toput] = index;
                 }
                 
@@ -31,6 +115,8 @@ public:
         }
         return -1;
     }
+
+
 
 
 
