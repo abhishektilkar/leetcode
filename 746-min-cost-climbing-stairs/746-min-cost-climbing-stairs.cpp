629abda052c430e2e342a9431f0e4a295b51fb9e
Abhishek Tilkar <84792257+abhishektilkar@users.noreply.github.com>
2022-07-24 16:47:18 +0530

    Time: 4 ms (89.81%), Space: 13.6 MB (77.69%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -4,17 +4,16 @@ public:
         
         
         int n = cost.size();
-        vector<int> v(n,0);
+        
+        int x=cost[n-2],y=cost[n-1],z;
         
         
-        for(int i=n-1;i>=0;i--) {
-            if(i == n-1 || i == n-2) {
-                v[i] = cost[i];
-                continue;
-            }
-            v[i] = cost[i] + min(v[i+1],v[i+2]);
+        for(int i=n-3;i>=0;i--) {
+            z = y;
+            y = x;
+            x = cost[i] + min(y,z);
         }
         
-        return min(v[0],v[1]);
+        return min(x,y);
     }
 };
