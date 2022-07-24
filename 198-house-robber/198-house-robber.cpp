6e35ee5b8786aeb1468443c73417af019ba7fee0
Abhishek Tilkar <84792257+abhishektilkar@users.noreply.github.com>
2022-07-24 23:18:46 +0530

    Time: 3 ms (54.07%), Space: 7.5 MB (94.89%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,43 +1,21 @@
 class Solution {
 public:
-    int val(vector<vector<int>> &v, vector<int>& nums, int i, int l) {
-        
-        
-        
-        int n = nums.size();
-        if(n <= i) {
-            return 0;
-        }
-        if(v[i][l] != -1) {
-            return v[i][l];
-        }
-        
-        if(n-1 == i && l) {
-            return v[i][l] = 0;
-        }
-        else if(i == n-1 && !l) {
-            return v[i][l] = nums[i];
-        }
-
-        
-        
-        int ai = 0;
-        if(l == 0) {
-            ai = nums[i] + val(v,nums,i+1,1);
-        }
-        ai = max(ai,val(v,nums,i+1,0));
-        
-
-        
-        
-        return v[i][l] = ai;
-    }
     int rob(vector<int>& nums) {
+        
+        int previ_1 = nums[0];
+        int previ_2 = 0,q;
         int n = nums.size();
+
         
-        vector<vector<int>> v(n,vector<int> (2,-1));
-        
-        return val(v, nums, 0, 0);
-        
+        for(int i=1;i<n;i++) {
+            
+
+            q = max(nums[i]+previ_2, previ_1);
+            previ_2 = previ_1;
+            previ_1 = q;
+            
+
+        }
+        return previ_1;
     }
 };
