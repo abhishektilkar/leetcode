c0b86f5e790e79c9fc9b473828fb59ee3140de07
Abhishek Tilkar <84792257+abhishektilkar@users.noreply.github.com>
2022-07-25 22:13:11 +0530

    Time: 425 ms (33.52%), Space: 17.3 MB (26.69%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        
+        
+
+        
+        int n = nums.size();
+        vector<int> v(n,0);
+        
+        for(int  i=n-1;i>=0;i--) {
+            
+            if(i == n-1) {v[i] = 0; continue;}
+            
+
+
+            
+            // long //
+            int val = INT_MAX;
+            for(int j=1;j<=nums[i] && i+j < n;j++) {
+                val = min(val, v[i+j]);
+            }
+            
+
+            
+            
+            
+            
+            
+            
+
+            if(val != INT_MAX) v[i] = val+1;
+            else v[i] = INT_MAX;
+        }
+        
+        
+        return v[0];
+    }
+
+};
