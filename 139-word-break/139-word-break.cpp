0f002e42a8643d27ffdf8ab82d49c5c1e33f89b0
Abhishek Tilkar <84792257+abhishektilkar@users.noreply.github.com>
2022-08-01 00:00:55 +0530

    Time: 41 ms (23.17%), Space: 15.5 MB (15.22%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -2,42 +2,6 @@ class Solution {
 public:
     vector<int> v;
     
-    bool isThereTo(string s,int i, unordered_map<string,int> map) {
-        
-        
-        
-        
-        
-        
-        
-        int n = s.size();
-        
-        if(i >= n) return 1;
-        
-        
-        if(v[i] != -1) {
-        
-            return (bool)(v[i]);
-        
-        }
-        
-        string string1 = s.substr(i,301);
-
-        
-        
-        
-        for(int j=0;j<n;j++) {
-            
-            string a = string1.substr(0,j+1);
-            
-            if(map[a] > 0 && isThereTo(s,i+j+1,map)) return v[i] = 1;
-
-            
-        }
-        
-        return v[i] = 0;
-        
-    }
     bool wordBreak(string s, vector<string>& wordDict) {
         
         
@@ -46,17 +10,33 @@ public:
         
         
         int n = s.size();
-        v.resize(n,-1);
+        v.resize(n,0);
         
         unordered_map<string,int> map;
         for(string a : wordDict) map[a]++;
         
         
-        return isThereTo(s,0,map);
+        for(int i=n-1;i>=0;i--) {
+            for(int j=i;j<n;j++) {
+                
+                
+                if(map[s.substr(i,j-i+1)] > 0 && (j >= n-1 || v[j+1] == 1)) {
+                    v[i] = 1;
+                    break;
+                    
+                }
+            }
+        }
+        // ||||| cout << v[7] << " "; ///|||||
+        
+        return v[0];
         
         
         
     }
+
+
+
 
 
 
